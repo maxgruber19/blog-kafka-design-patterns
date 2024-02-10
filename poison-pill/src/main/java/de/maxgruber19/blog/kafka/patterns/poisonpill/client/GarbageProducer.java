bbd4e4e5cdff304b56350998fff8373dc1e2d4db
Max Gruber <maxgruber19@gmail.com>
2024-02-10 20:48:56 +0100

    feat/lets-go added auto garbage producer to poisonpill to alawys have some broken messages in the sourcetopic

---
+++ poison-pill/src/main/java/de/maxgruber19/blog/kafka/patterns/poisonpill/client/GarbageProducer.java
@@ -0,0 +1,32 @@
+package de.maxgruber19.blog.kafka.patterns.poisonpill.client;
+
+import de.maxgruber19.blog.kafka.patterns.poisonpill.model.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.UUID;
+
+@Slf4j
+@Component
+class GarbageProducer {
+
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Scheduled(fixedRate = 15000)
+    public void produceTestMessage() {
+        log.info("sending garbage");
+        ProducerRecord<Integer, String> record = new ProducerRecord<>("order-events-ingoing", "Let me check how fault tolerant you really are, kafka.");
+        kafkaTemplate.send(record);
+        log.debug("sent garbage {}", record);
+    }
+
+}
